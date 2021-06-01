86060e166730ff525690247251e31c9988825a83
Kapil Rana <rana.kapil9@gmail.com>
2021-06-01 18:04:53 +0530

    Created file
    arduino blink file

---
+++ blink/blink.ino
@@ -0,0 +1,19 @@
+
+int led =13;
+int green=12;
+void setup()
+{
+ pinMode(led,OUTPUT);
+ pinMode(green,OUTPUT);
+}
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+   digitalWrite(green,HIGH);
+  delay(1000);
+  digitalWrite(green,LOW);
+  delay(1000);
+
+}
