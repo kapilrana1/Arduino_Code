06cf502674a793d809ad36f2adb6457dccb10d45
Kapil Rana <rana.kapil9@gmail.com>
2021-06-06 04:30:52 +0530

    Create Push_button.ino

---
+++ Push_button/Push_button.ino
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+const int ledpin = 2; // initializing pin number 2 to the LED  
+const int buttonpin = 4; // initializing pin number 4 to the button  
+int buttonState = 0;  
+void setup()  
+{  
+  Serial.begin(9600);  
+  pinMode(buttonpin, INPUT);  
+  pinMode(ledpin, OUTPUT);  
+}  
+void loop()  
+{  
+  // read the state of the pushbutton value  
+  buttonState = digitalRead(buttonpin);  
+  // check if pushbutton is pressed.  if it is, the  
+  // buttonState is HIGH  
+  if (buttonState == HIGH) {  
+    // turn LED on  
+    digitalWrite(ledpin, HIGH);  
+    Serial.println("LED is ON");  
+    //When we press the button, it will print LED is ON.  
+delay ( 500);  
+  }   
+  else   
+  {  
+    // turn LED off  
+    digitalWrite(ledpin, LOW);  
+    Serial.println("LED is OFF"); // When we press the button, it will print LED is OFF.  
+  }  
+delay ( 500);  
+}  
