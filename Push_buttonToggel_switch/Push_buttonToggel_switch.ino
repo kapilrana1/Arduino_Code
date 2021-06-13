bd4654e6ee28a1aeb3fa1164d2fef65d98c098fe
Kapil Rana <rana.kapil9@gmail.com>
2021-06-13 22:34:41 +0530

    update

--- Push_buttonToggel_switch/Push_buttonToggel_switch.ino
+++ Push_buttonToggel_switch/Push_buttonToggel_switch.ino
@@ -1,27 +1,20 @@
-// toggel switch push button
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int Switch = 2, LED = 3;
+int state = 0, LEDstate=0;
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-
-void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+void setup()
+{
+  pinMode(Switch, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
-
-void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+void loop()
+{
+  if (state == 0 && digitalRead(Switch) == HIGH) {
+    state = 1;
+    LEDstate=!LEDstate;
   }
+  if (state == 1 && digitalRead(Switch) == LOW) {   
+    state = 0;
+  }
+   digitalWrite(LED, LEDstate);
 }
