b009325641dd76da289de22897135a996ef57c19
Kapil Rana <rana.kapil9@gmail.com>
2021-06-15 11:14:24 +0530

    photo_cell Code

---
+++ Photocell_WithArduino/Photocell_WithArduino.ino
@@ -0,0 +1,16 @@
+int switch1 = 2;
+int led_pin = 9;
+
+void setup() {
+  pinMode(switch1, INPUT_PULLUP);
+   pinMode(led_pin, OUTPUT);
+  }
+void loop() {
+  if(digitalRead(switch1) == HIGH){
+    digitalWrite(led_pin, HIGH);
+  }
+  if(digitalRead(switch1) == LOW){
+    digitalWrite(led_pin, LOW);
+     }
+
+}
