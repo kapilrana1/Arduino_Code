11ee8b2511f76a5b82abbcaa86e840f663a45b07
Kapil Rana <rana.kapil9@gmail.com>
2021-07-25 19:46:15 +0530

    if && statements
    New 25_2021

---
+++ If_And_Statments/If_And_Stetments.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+int buttonL = 6;
+int buttonR = 7;
+int redL = 1;
+int redR = 2;
+int blue = 3;
+void setup() {
+  pinMode(buttonL,INPUT);
+  pinMode(buttonR,INPUT);
+  pinMode(redL,OUTPUT);
+  pinMode(redR,OUTPUT);
+  pinMode(blue,OUTPUT);
+}
+void loop() {
+  if (digitalRead(buttonL)==HIGH && digitalRead(buttonR)==HIGH)
+  {digitalWrite(blue,HIGH);
+  }
+  else
+    {
+      digitalWrite(blue,LOW);
+    }
+    if(digitalRead(buttonL)==HIGH && digitalRead(buttonR)==LOW);
+    {
+      digitalWrite(redL,HIGH);
+      digitalWrite(redR,LOW);
+    }
+    if(digitalRead(buttonL)==LOW && digitalRead(buttonR)==HIGH);
+    {
+      digitalWrite(redR,HIGH);
+      digitalWrite(redL,LOW);
+    }
+  }
